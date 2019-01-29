94841827fe7a8fbf90fd190438f7ccb999c64a63
Kill_Rill <hizenberg228@mai.ru>
2019-01-29 20:36:18 +0300

    валидность фикстуры

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,12 +1,8 @@
 
 
 def test_del_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.del_first_group()
-    app.session.logout()
 
 
 # def test_del_first_contact(app):
-#     app.session.login(username="admin", password="secret")
 #     app.contact.del_first_contact()
-#     app.session.logout()
