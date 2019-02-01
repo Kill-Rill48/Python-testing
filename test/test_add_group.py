c3bb2afe10af41f84e3ba440420bdfd9d749287b
Kill_Rill <hizenberg228@mai.ru>
2019-02-01 22:27:19 +0300

    проверки на правльность добавления и удаления групп, а также модификации

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,17 @@ from model.contact import Contact
 from model.group import Group
 
 
-# def test_add_group(app):
-#     app.group.create_group(Group(name="asdsdfsdf", header="fasdfasdf", footer="asdasd"))
-#
-#
-# def test_add_empty_group(app):
-#     app.group.create_group(Group(name="", header="", footer=""))
+def test_add_group(app):
+    old_groups = app.group.get_group_list()
+    app.group.create_group(Group(name="asdsdfsdf", header="fasdfasdf", footer="asdasd"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
+def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
+    app.group.create_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_contact(app):
     app.contact.create_contact(Contact(firstname="alex", middlename="andreev", lastname="igorevich", nickname="w3qxst1ck", home="Izhevsk", email="w3qxst1ck@mail.ru"))
--- test/test_modify_gtoup.py
+++ test/test_modify_gtoup.py
@@ -4,12 +4,16 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create_group(Group(name="Host_pidor"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create_group(Group(name="Host_pidor"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
