c7288e52bb997d1631eac803b56e86c05022e0be
Kill_Rill <hizenberg228@mai.ru>
2019-02-14 11:08:24 +0300

    выбор исполняемого барузера

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,6 +6,9 @@ import string
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "
+    symbols2 = string.ascii_letters + string.digits
+    if prefix == "email" or prefix == "email2" or prefix == "email3":
+        return prefix + "".join([random.choice(symbols2) for i in range(random.randrange(maxlen))]) + "@mail.ru"
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -13,7 +16,7 @@ testdata = [Contact(firstname="", middlename="", lastname="", nickname="", email
     Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 15), lastname=random_string("lastname", 12),
             nickname=random_string("nickname", 10), email=random_string("email", 10), email2=random_string("email2", 10),
             email3=random_string("email3", 10), homephone=random_string("homephone", 10), workphone=random_string("workphone", 10))
-    for i in range(3)]
+    for i in range(2)]
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
